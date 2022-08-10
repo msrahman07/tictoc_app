8e4b87414e0ed73abbae996538492bc6c8934635
Shahriar <msrahman@sfu.ca>
2022-08-09 21:39:03 -0700

    added videocard, googleauth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 import Logo from '../utils/tiktik-logo.png';
+import { createOrGetUser } from '../utils';
+import useAuthStore from '../store/authStore';
 
 const Navbar = () => {
+  const { userProfile, addUser } = useAuthStore();
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
         <Link href="/">
@@ -21,6 +24,17 @@ const Navbar = () => {
                 />
             </div>
         </Link>
+        <div>Search</div>
+        <div>
+          {userProfile ? (
+            <div>{userProfile.userName}</div>
+          ): (
+            <GoogleLogin 
+              onSuccess={(response) => createOrGetUser(response, addUser)}
+              onError={() => console.log("error")}
+            />
+          )}
+        </div>
     </div>
   )
 }
--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -11,54 +11,93 @@ interface IProps {
     post: Video
 }
 
-const VideoCard : NextPage<IProps> = ({ post }) => {
-  return (
-    <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
-        <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
-            <div className='md:w-16 md:h-16 w-10 h-10'>
-                <Link href="/">
-                    <>
-                        <Image 
-                            width={62}
-                            height={62}
-                            className="rounded-full"
-                            src={post.postedBy.image}
-                            alt="profile photo"
-                            layout='responsive'
-                        />    
-                    </>
-                </Link>
+const VideoCard: NextPage<IProps> = ({ post }) => {
+    const [isHover, setIsHover] = useState(false);
+    const [playing, setPlaying] = useState(false);
+    const [isVideoMuted, setisVideoMuted] = useState(false);
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    const onVideoPress = () => {
+        if(playing) {
+            videoRef?.current?.pause();
+            setPlaying(false);
+        }else{
+            videoRef?.current?.play();
+            setPlaying(true);
+        }
+    };
+    return (
+        <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
+            <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+                <div className='md:w-16 md:h-16 w-10 h-10'>
+                    <Link href="/">
+                        <>
+                            <Image
+                                width={62}
+                                height={62}
+                                className="rounded-full"
+                                src={post.postedBy.image}
+                                alt="profile photo"
+                                layout='responsive'
+                            />
+                        </>
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/">
+                        <div className='flex items-center gap-2'>
+                            <p className='flex gap-2 iterms-center md:text-md font-bold text-primary'>{post.postedBy.userName} {` `}
+                                <GoVerified className='text-blue-400 text-md' />
+                            </p>
+                            <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>{post.postedBy.userName} </p>
+                        </div>
+                    </Link>
+                </div>
             </div>
-            <div>
-                <Link href="/">
-                    <div className='flex items-center gap-2'>
-                        <p className='flex gap-2 iterms-center md:text-md font-bold text-primary'>{post.postedBy.userName} {` `}
-                            <GoVerified className='text-blue-400 text-md'/>
-                        </p>
-                        <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>{post.postedBy.userName} </p>
-                    </div>
-                </Link>
-            </div>
-        </div>
-        <div className='lg:ml-20 flex gap-4 relative'>
-            <div 
-                className='rounded-3xl'
-                onMouseEnter={() => {}}
-                onMouseLeave={() => {}}
+            <div className='lg:ml-20 flex gap-4 relative'>
+                <div
+                    className='rounded-3xl'
+                    onMouseEnter={() => setIsHover(true)}
+                    onMouseLeave={() => setIsHover(false)}
                 >
-                <Link href="/">
-                    <video
-                        loop
-                        src={post.video.asset.url}
-                        className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[528px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
-                    >
-                        
-                    </video>
-                </Link>
+                    <Link href="/">
+                        <video
+                            loop
+                            onClick={onVideoPress}
+                            muted={isVideoMuted}
+                            ref={videoRef}
+                            src={post.video.asset.url}
+                            className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[528px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
+                        >
+
+                        </video>
+                    </Link>
+                    {isHover && (
+                        <div className='absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] lg:w-[600px] p-3'>
+                            {playing ? (
+                                <button onClick={onVideoPress}>
+                                    <BsFillPauseFill className='text-black text-2xl lg:text-4xl' />
+                                </button>
+                            ) : (
+                                <button onClick={onVideoPress}>
+                                    <BsFillPlayFill className='text-black text-2xl lg:text-4xl' />
+                                </button>
+                            )}
+                            {isVideoMuted ? (
+                                <button onClick={() => setisVideoMuted(false)}>
+                                    <HiVolumeOff className='text-black text-2xl lg:text-4xl' />
+                                </button>
+                            ) : (
+                                <button onClick={() => setisVideoMuted(true)}>
+                                    <HiVolumeUp className='text-black text-2xl lg:text-4xl' />
+                                </button>
+                            )}
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default VideoCard
