2fd7a485dcf61ded1dcd33561ed79ec7bd184da9
Shahriar <msrahman@sfu.ca>
2022-08-10 19:59:39 -0700

    added upload functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,30 +11,59 @@ import { createOrGetUser } from '../utils';
 import useAuthStore from '../store/authStore';
 
 const Navbar = () => {
-  const { userProfile, addUser } = useAuthStore();
+  const { userProfile, addUser, removeUser } = useAuthStore();
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
-        <Link href="/">
-            <div className='w-[100px] md:w-[130px]'>
-                <Image 
-                    className='cursor-pointer'
-                    src={Logo}
-                    alt='tictoc'
-                    layout='responsive'
-                />
-            </div>
-        </Link>
-        <div>Search</div>
-        <div>
-          {userProfile ? (
-            <div>{userProfile.userName}</div>
-          ): (
-            <GoogleLogin 
-              onSuccess={(response) => createOrGetUser(response, addUser)}
-              onError={() => console.log("error")}
-            />
-          )}
+      <Link href="/">
+        <div className='w-[100px] md:w-[130px]'>
+          <Image
+            className='cursor-pointer'
+            src={Logo}
+            alt='tictoc'
+            layout='responsive'
+          />
         </div>
+      </Link>
+      <div>Search</div>
+      <div>
+        {userProfile ? (
+          <div className='flex gap-5 md:gap-10'>
+            <Link href="/upload">
+              <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                <IoMdAdd className='text-xl' /> {` `}
+                <span className='hidden md:block'>Upload</span>
+              </button>
+            </Link>
+            {userProfile.image && (
+              <Link href="/">
+                <>
+                  <Image
+                    width={40}
+                    height={40}
+                    className="rounded-full curson-pointer"
+                    src={userProfile.image}
+                    alt="profile photo"
+                  />
+                </>
+              </Link>
+            )}
+            <button 
+              type='button'
+              className='px-2'
+              onClick={() => {
+                googleLogout();
+                removeUser();
+              }}>
+              <AiOutlineLogout color='red' fontSize={21} />
+            </button>
+          </div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => createOrGetUser(response, addUser)}
+            onError={() => console.log("error")}
+          />
+        )}
+      </div>
     </div>
   )
 }
